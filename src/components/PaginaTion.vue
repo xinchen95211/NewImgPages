e6c1a2677b83e24fe2db9c88921f17bbb2b79102
xinchen95211 <76147759+xinchen95211@users.noreply.github.com>
2023-09-21 17:00:35 +0800

    Update PhotoMain.vue

--- src/components/PaginaTion.vue
+++ src/components/PaginaTion.vue
@@ -1,7 +1,8 @@
 <template>
+
   <el-pagination
       :page-size="24"
-      :pager-count="11"
+      :pager-count=pagerCount
       background layout="prev, pager, next"
       :total="totalCount"
       :current-page="currentPage"
@@ -16,14 +17,32 @@ export default {
   name: "PaginaTion",
   data(){
     return{
-
+      pagerCount:15,
     }
   },
+  mounted() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize(); // 初始执行一次回调函数
+  },
+
   methods:{
     pageTurning(e){
       console.log(e)
       this.$emit("pageTurning",e)
+    },
+    handleResize() {
+      // 获取屏幕宽度
+      const screenWidth = window.innerWidth || document.documentElement.clientWidth;
+      // 检查屏幕宽度是否小于等于575px
+      if (screenWidth <= 575) {
+        this.pagerCount = 5;
+      } else if(screenWidth >= 576 && screenWidth <= 856){
+        this.pagerCount = 9;
+      } else {
+        this.pagerCount = 15;
+      }
     }
+
   },
   props:{
     totalCount:Number,
