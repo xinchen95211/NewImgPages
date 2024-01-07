acd6a9918307adc557a68a4383c8489cb9b42012
xinchen95211 <76147759+xinchen95211@users.noreply.github.com>
2024-01-07 12:03:34 +0800

    添加登陆页面

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'htto://localhost:8888',
+        target: 'http://localhost:8888',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
