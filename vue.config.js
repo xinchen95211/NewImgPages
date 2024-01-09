d88c3e2a91855aaf7bb245bc9a7f42c86bd2df01
xinchen95211 <76147759+xinchen95211@users.noreply.github.com>
2024-01-09 12:05:41 +0800

    添加登陆页面

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 
-
+module.exports ={
+  publicPath: '/',
+}
 module.exports = defineConfig({
-  PublicPath:'./',
   transpileDependencies: false,
   devServer: {
     proxy: {
