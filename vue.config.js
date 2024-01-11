54e6c5304eb7eba8f3d680a6ff4e06f3788d9102
xinchen95211 <76147759+xinchen95211@users.noreply.github.com>
2024-01-11 14:47:00 +0800

    x修正一下

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: false,
+  transpileDependencies: true,
   devServer: {
     proxy: {
       '/api': {
         target: 'https://frp-hat.top:49728',
         // target:'http://192.168.0.108:9999',
         changeOrigin: true,
+        secure:false,
         pathRewrite: {
           '^/api': ''
         }
